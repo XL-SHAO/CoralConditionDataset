16098c0dd77f214cbb64295553e76c545e1f94ba
XinleiShao <yuishaoxinlei@gmail.com>
2024-06-07 14:10:28 +0900

    commit of local code

---
+++ code/sh_file/train_swintransformer_uda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -jc gs-container_g1.24h
+#$ -ac d=nvcr-pytorch-2010,d_shm=64G
+#$ -N train_SwinT_UDA
+
+. ~/net.sh
+
+/home/songjian/.conda/envs/openmmlab/bin/python script/train_swintransformer_uda.py
